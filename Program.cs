72ce98f85e9967ffd931cbf1e2180f0832922a98
Шарафутдинов Евгений Асгатович <evgen.sh77@mail.ru>
2022-11-27 20:58:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generic
+{
+    class Generic<T>
+    {
+        public T field;
+
+        public void Method(T param)
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
